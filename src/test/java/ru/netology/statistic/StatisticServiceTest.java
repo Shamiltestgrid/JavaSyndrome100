54a34bd8bd165d07c1be1fcabcb3ed2412d23aaa
Shamil Garifullin <supermariokazan@gmail.com>
2020-10-08 12:54:00 +0300

    Commit

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -9,8 +9,8 @@ class StatisticServiceTest {
     @Test
     void findMax() {
         StatisticService service = new StatisticService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
